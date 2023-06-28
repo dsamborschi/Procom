a207b0e960bf3007eb632327f7b92d90a14e559a
dsamborschi <dsamborschi@yahoo.com>
2023-06-28 14:09:52 -0400

    chore:add dbcontext extenions

---
+++ Procom.Api/Extensions/Extenions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace Procom.Api.Extensions
+
+{
+    internal static class Extensions
+    {
+        public static IServiceCollection AddDbContexts(this IServiceCollection services, IConfiguration configuration)
+        {
+            static void ConfigureSqlOptions(SqlServerDbContextOptionsBuilder sqlOptions)
+            {
+                sqlOptions.MigrationsAssembly(typeof(Program).Assembly.FullName);
+                sqlOptions.EnableRetryOnFailure(maxRetryCount: 15, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null);
+            };
+
+            services.AddDbContext<ProcomContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("OrderingDB"), ConfigureSqlOptions);
+            });
+
+
+
+            return services;
+        }
+    }
+}
---
+++ Procom.Api/ProcomContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Procom.Api
+{
+    internal class ProcomContext:DbContext
+    {
+        public ProcomContext(DbContextOptions<ProcomContext> options) : base(options) { }
+    }
+}
